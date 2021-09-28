da48c825857dc8e716bcf146ae9b9bc11d55142b
AramJu <aaadkfka@naver.com>
2021-09-28 19:53:26 +0900

    Signin screen test

---
+++ app/src/main/java/com/example/composable/model/identity/UserInfo.kt
@@ -0,0 +1,6 @@
+package com.example.composable.model.identity
+
+data class UserInfo (
+    var userLoggedIn: Boolean = false,
+    var userName: String = ""
+)
--- app/src/main/java/com/example/composable/ui/pages/Setting.kt
+++ app/src/main/java/com/example/composable/ui/pages/Setting.kt
@@ -1,13 +1,12 @@
 package com.example.composable.ui.pages
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import com.example.composable.TopBar
+import com.example.composable.model.identity.UserInfo
 
 @Composable
 fun Setting() {
     Column() {
-        Text(text = "Setting")
+        AppScreen(UserInfo())
     }
 }
